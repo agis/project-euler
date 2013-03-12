23f3743e9234d8cf9324184bf8c4092ccbd18ae2
Agis Anastasopoulos <corestudiosinc@gmail.com>
2013-03-12 21:48:31 +0200

    Problem 6: add a comment

--- problem6.erl
+++ problem6.erl
@@ -1,7 +1,8 @@
 -module(problem6).
 -compile(answer).
 
+% Using sum, seq & map BIFs
+
 answer() ->
   lists:sum(lists:seq(1, 100)) * lists:sum(lists:seq(1, 100)) -
   lists:sum(lists:map(fun(X) -> X*X end, lists:seq(1, 100))).
-
